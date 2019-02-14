64283f3a3c0f52b03f644c292fde420ea57d89e6
Assimh <hanifaassim95@gmail.com>
2019-02-14 10:35:30 +0100

    Test Unitaire ReceiptPrinter

--- src/test/java/esiea/model/SupermarketTest.java
+++ src/test/java/esiea/model/SupermarketTest.java
@@ -1,7 +1,7 @@
 package esiea.model;
 
 import org.junit.jupiter.api.Test;
-
+import esiea.ReceiptPrinter;
 import org.assertj.core.api.Assertions;
 
 public class SupermarketTest {
@@ -126,4 +126,21 @@ public class SupermarketTest {
         Assertions.assertThat(receipt.getTotalPrice()).isEqualTo(3.98);
 
      }
+
+   //Test ReceiptPrinter
+   @Test
+   public void testReceiptPrinter() {
+      SupermarketCatalog catalog = new FakeCatalog();
+      Product apples = new Product("apples", ProductUnit.Kilo);
+      catalog.addProduct(apples, 1.99);
+
+      ShoppingCart cart = new ShoppingCart();
+      cart.addItemQuantity(apples, 2);
+
+      Teller teller = new Teller(catalog);
+       
+      Receipt receipt = teller.checksOutArticlesFrom(cart);
+      ReceiptPrinter receiptprinter = new ReceiptPrinter();
+      Assertions.assertThat(receiptprinter.printReceipt(receipt)).isNotBlank();
+   }
 }
