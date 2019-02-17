aa31f62d9e55384e02ec4f185ea5a7fab9403f59
Assimh <hanifaassim95@gmail.com>
2019-02-17 14:49:39 +0100

    Test ProductTest

---
+++ src/test/java/esiea/model/ProductTest.java
@@ -0,0 +1,60 @@
+package esiea.model;
+
+import org.junit.jupiter.api.Test;
+import esiea.model.ReceiptItem;
+import org.assertj.core.api.Assertions;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class ProductTest {
+
+    @Test
+    public void testGetName(){
+
+        Product apples = new Product("apples", ProductUnit.Kilo);
+
+        Assertions.assertThat(apples.getName()).isEqualTo("apples");
+
+    }
+
+    @Test
+    public void testGetUnit(){
+
+        Product apples = new Product("apples", ProductUnit.Kilo);
+
+        Assertions.assertThat(apples.getUnit()).isEqualTo(ProductUnit.Kilo);
+    }
+
+     @Test
+    public void testEquals () {
+
+        Product apples = new Product("apples", ProductUnit.Kilo);
+
+        Discount discount = new Discount(apples, "Apples Discount", 0.199);
+
+        Product banane = new Product("banane", ProductUnit.Kilo);
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        Product apples1 = new Product("apples", ProductUnit.Kilo);
+
+        Assertions.assertThat(apples.equals(apples)).isEqualTo(true);
+        Assertions.assertThat(apples.equals(null)).isEqualTo(false);
+
+        Assertions.assertThat(apples.equals(discount)).isEqualTo(false);
+        Assertions.assertThat(apples.equals(banane)).isEqualTo(false);
+        Assertions.assertThat(apples.equals(toothbrush)).isEqualTo(false);
+        Assertions.assertThat(apples.equals(apples1)).isEqualTo(true);
+
+
+    }
+
+    @Test
+    public void testHashCode(){
+
+        Product apples = new Product("apples", ProductUnit.Kilo);
+
+        Assertions.assertThat(apples.hashCode()).isEqualTo(Objects.hash("apples", ProductUnit.Kilo));
+    }
+
+}
